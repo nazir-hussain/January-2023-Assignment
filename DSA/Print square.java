6cc64b09a8867a568bac300d0d96afc9b2f06e34
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2023-02-03 22:22:12 +0530

    Update Print square.java

--- DSA/Print square.java
+++ DSA/Print square.java
@@ -1,1 +1,21 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
 
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc=new Scanner(System.in);
+
+// System.out.println(“Enter your value”);
+
+    int n=sc.nextInt();
+
+    for(int i=1;i<=n;i++){
+
+        // System.out.println(i+" "+i*i+" "+i*i*i);
+        System.out.println(i*i);
+        }
+    }
+}
