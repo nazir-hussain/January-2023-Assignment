24753f6b2560d8cfff09cc6cd7a1a2c4af1d6935
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2023-02-03 22:15:00 +0530

    Update Consonants.java

--- DSA/Consonants.java
+++ DSA/Consonants.java
@@ -1,1 +1,22 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
 
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        char ch;
+
+        for(int i=0; i<s.length(); i++){
+            ch = s.charAt(i);
+
+            if(ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u'){
+                System.out.print(s.charAt(i));
+            }
+        }
+    }
+}
