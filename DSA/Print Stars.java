36cbbaad70feb99fd84a31b1b54095f45fa6b493
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2023-02-07 22:55:19 +0530

    Update Print Stars.java

--- DSA/Print Stars.java
+++ DSA/Print Stars.java
@@ -1,1 +1,14 @@
+static void verticalFive(){
+//Enter your code here
+    for(int i=0; i<5; i++){
+        System.out.println("*");
+    }
+    
+}
 
+static void horizontalFive(){
+    //Enter your code here
+    for(int i =0; i<5; i++){
+        System.out.print("* ");
+    }
+}
