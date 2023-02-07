d03d89786c500bbc9c53e0add84ff4023d8da55b
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2023-02-07 22:34:12 +0530

    Update Week Name.java

--- DSA/Week Name.java
+++ DSA/Week Name.java
@@ -1,1 +1,40 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
 
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner in = new Scanner(System.in);
+    int dayNum;
+    // System.out.print("Enter a day number (1-7): ");
+    dayNum = in.nextInt();
+        switch(dayNum) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;       
+          default:
+            System.out.println("Invalid day number.");
+            break;                      
+        }
+    }
+}
