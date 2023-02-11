9633c968605857cf9f8618e02dedef581813bd23
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2023-02-11 21:34:27 +0530

    Update Triangle (right angle).java

--- DSA/Triangle (right angle).java
+++ DSA/Triangle (right angle).java
@@ -1,1 +1,10 @@
+static void printTriangle(){
+// Your code here
+    for(int i=1; i<=5; i++){
+        for(int j=1; j<=i; j++){
+            System.out.print("*" +" ");
+        }
+        System.out.println();
+    }
 
+}
