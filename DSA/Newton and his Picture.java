b837f2f6f85ee7068348cfa8cf6784cf8851ea64
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2023-02-11 21:57:51 +0530

    Update Newton and his Picture.java

--- DSA/Newton and his Picture.java
+++ DSA/Newton and his Picture.java
@@ -1,1 +1,32 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
 
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int L = sc.nextInt();
+        int N = sc.nextInt();
+        int[][] photos = new int[N][2];
+
+        for(int i=0; i<N; i++){
+            photos[i][0] = sc.nextInt();
+            photos[i][1] = sc.nextInt();
+        }
+
+        for(int i=0; i<N; i++){
+            int W = photos[i][0];
+            int H = photos[i][1];
+
+            if(Math.min(W, H) < L){
+                System.out.println("UPLOAD ANOTHER");
+            }else if(W == H){
+                System.out.println("ACCEPTED");
+            }else{
+                System.out.println("CROP IT");
+            }
+        }
+    }
+}
