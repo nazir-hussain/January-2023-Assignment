a173375aa25aede47fd7c3d088f0590594964168
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2023-02-11 21:29:16 +0530

    Update Palindrome Number.java

--- DSA/Palindrome Number.java
+++ DSA/Palindrome Number.java
@@ -1,1 +1,19 @@
+static void isPalindrome(int N)
+	{
+	   // Your code here
+	    int r,sum=0,temp;    
+  		// int n=454;//It is the number variable to be checked for palindrome  
+  
+  		temp=N;    
+  		while(N>0){    
+   		r=N%10;  //getting remainder  
+   		sum=(sum*10)+r;    
+   		N=N/10;    
+  		}
 
+  		if(temp==sum){
+   			System.out.println("True");    
+		}else{    
+   			System.out.println("False");
+		}
+	}
