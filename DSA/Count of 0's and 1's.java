39819dff1617f36060dce97992aa3c3adccc3584
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2023-02-25 22:10:26 +0530

    Update Count of 0's and 1's.java

--- DSA/Count of 0's and 1's.java
+++ DSA/Count of 0's and 1's.java
@@ -1,1 +1,23 @@
 
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            int input = scanner.nextInt();
+
+            sum = sum + input;
+        }
+
+        System.out.println(sum + " " + (n - sum));
+    }
+}
