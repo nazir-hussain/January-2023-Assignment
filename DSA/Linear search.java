acc1fcd9f973d41a3bcb8652b0a21590e6f28942
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2023-02-25 22:09:10 +0530

    Update Linear search.java

--- DSA/Linear search.java
+++ DSA/Linear search.java
@@ -1,1 +1,36 @@
 
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner scanner = new Scanner(System.in);
+
+        // Read the input
+        int n = scanner.nextInt();
+        int x = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        // Perform the linear search
+        boolean found = false;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == x) {
+                found = true;
+                break;
+            }
+        }
+
+        // Print the result
+        if (found) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
